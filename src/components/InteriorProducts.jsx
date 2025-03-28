294b7f44944ed6cae2a4d7566aad42da68a210ff
purnale <bhaktipurnale89@gmail.com>
2025-03-28 23:35:13 +0530

    add

--- src/components/InteriorProducts.jsx
+++ src/components/InteriorProducts.jsx
@@ -8,14 +8,29 @@ import product6 from "../assets/product6.png";
 
 const InteriorProducts = () => {
   return (
-    <div>
-      <div className="items-center justify-center text-center mt-16">
+    <div className="">
+      <div className="flex items-center justify-center text-center mt-16">
         <h1 className="font-medium text-4xl text-[#241330]">Our Interior Design Products</h1>
       </div>
-      <div>
-         <img className="w-[313px] h-[331px]"  src={product1} alt="" />
+      <div className="mt-10 ">
+         <img className="w-[313px] h-[331px] "  src={product1} alt="" />
       </div>
-    </div>
+      <div className="">
+        <img className="w-[318px] h-[330px]" src={product2} alt="" />
+      </div>
+      <div className="flex w-full object-cover">
+        <img className="w-[452px] h-[676px]" src={product3} alt="" />
+      </div>
+      <div className="">
+        <img className="w-[330px] h-[331px]" src={product4} alt="" />
+      </div>
+      <div className="">
+        <img className="w-[330px] h-[330px]" src={product5} alt="" />
+      </div>
+      <div className="">
+        <img className="w-[451px] h-[676px]" src={product6} alt="" />
+      </div>
+      </div>
   );
 };
 
